61053e9ff3f3e18b89f32aa6f88666081aef1211
Amitha Ajithkumar <amitha.ajithkumar4@gmail.com>
2026-04-07 23:07:47 -0400

    added changes to list

--- crypto.py
+++ crypto.py
@@ -5,34 +5,36 @@ def encrypt_text():
         plainText = file.read()
         stringList = list(plainText)  #converting the string to char first
         #print stringList
-        tempList = [] #creating an empty list to store ascii values
         encryptedString = ""
         i = 0
         while i < 16: #looping through number of chars in my name
-            tempList.append(ord(stringList[i])) #adding ascii values to temp list
+            stringList[i] = (ord(stringList[i])) #adding ascii values to temp list
             i+=1
-        #print ("before encryption", tempList)
+        print ("before encryption", stringList)
         key = 5 #the chosen key for this project
         i = 0
         while i < 16:
-            tempList[i] = (tempList[i] + key) #shift 5 spaces forward
+            stringList[i] = (stringList[i] + key) #shift 5 spaces forward
             i+=1
-        chars  = [chr(n) for n in tempList]   #converting ascii values back to char 
+        chars  = [chr(n) for n in stringList]   #converting ascii values back to char 
         encryptedString = ''.join(chars) #joining chars back to string
         print ("Encrypted Text is: ", encryptedString)
-        return tempList #returning list of ascii to decrypt
+        return encryptedString #returning list of ascii to decrypt
 
 ########################### DECRYPTION ####################################
 
 def decrypt_text(inputKey, encryptedList):
     i = 0
-    tempList = []
+    encryptedChar = list(encryptedList)
     while i < 16:
-        encryptedList[i] = encryptedList[i] - inputKey #reversing encryption by moving 5 spaces back
-        tempList.append(encryptedList[i])
+        encryptedChar[i] = (ord(encryptedChar[i]))
         i+=1
-    #print("decrypted number", encryptedList)
-    chars  = [chr(n) for n in encryptedList] #converting ascii values to char
+    j = 0
+    while j < 16:
+        encryptedChar[j] = encryptedChar[j] - inputKey #reversing encryption by moving 5 spaces back
+        j+=1
+    print("decrypted number", encryptedChar)
+    chars  = [chr(n) for n in encryptedChar] #converting ascii values to char
     encryptedString = ''.join(chars)
     print ("Your decrypted text is: ", encryptedString)
 
@@ -42,11 +44,11 @@ def decrypt_text(inputKey, encryptedList):
 inputKey = 0
 
 while inputKey != 1:
-    encryptedList = encrypt_text()
+    encryptedString = encrypt_text()
     inputKey = int (input ("Enter your chosen key and 1 to exit : "))
     if inputKey == 5:
         print("Congratulations!")
-        decrypt_text(inputKey, encryptedList)
+        decrypt_text(inputKey, encryptedString)
         break
     if inputKey == 1:
         print("Thanks for guessing!")
